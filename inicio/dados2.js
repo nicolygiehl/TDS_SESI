2777a2386f525671110f422dbbba090d7066a46b
nicolygiehl <nicoly.giehl0@gmail.com>
2026-02-27 11:47:37 -0300

    tipos de dados em JS

---
+++ inicio/dados2.js
@@ -0,0 +1,13 @@
+//console.log(19.9 * 0.6);
+
+var preco = 19.9;
+var desconto = 0.6;
+var precoComDesconto = preco * (1 - desconto);
+//console.log(precoComDesconto);
+
+let nome = "Caderno";
+let categoria = "Papalaria";
+console.log("Produto: " + nome + 
+    ", categoria:" + categoria + 
+    ", preco:" + preco + 
+    ", desconto: " + desconto);
