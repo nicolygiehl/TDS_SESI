26dd3606883776e40212a0fc2f89e9d62fe906ef
nicolygiehl <nicoly.giehl0@gmail.com>
2026-03-27 11:29:55 -0300

    atividade5

---
+++ atividades/atividade3.js
@@ -0,0 +1,10 @@
+var idade =16;
+var convite = true;
+if(idade >= 18 &&possuiConvite == true) {
+    console.log("Você pode entrar");
+}else{
+    console.log("Você pode entrar")
+}
+
+let mensagem = idade >= 18? "Você pode entrar" : "Você pode entrar";
+console.log(mensagem);
---
+++ atividades/atividade4.js
@@ -0,0 +1,3 @@
+const preco = 100;
+let mensagem = preco >= 100? (preco - (preco * 0.10)):preco
+console.log(mensagem);
