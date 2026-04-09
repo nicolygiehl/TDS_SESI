8f5fb8fc7e85c8c71328911c4a83ee9ba315c039
nicolygiehl <nicoly.giehl0@gmail.com>
2026-04-09 12:10:33 -0300

    desafio_final

---
+++ atividades/desafio_final.js
@@ -0,0 +1,19 @@
+const estoque = ["monitor", "teclado", "mouse", "placa-mãe", "CPU"];
+
+estoque.push("placa de video");
+estoque.shift(estoque[0]);
+
+console.log(estoque[0]);
+console.log(estoque[1]);
+console.log(estoque[2]);
+console.log(estoque[3]);
+console.log(estoque[4]);  
+
+let clienteNome = "nicoly";
+let clienteIdade = 16;
+let possuiCupom = true
+
+function processarValor (valorTotal, cupomAtivo, quantidadeItens){
+    var ver
+}
+
